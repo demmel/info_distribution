5e1b2deef1503370aa8047a6173471eb38b4c6e6
David Emmel <dgemmel2@gmail.com>
2021-12-04 23:43:03 -0800

    Code organization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::{ImageBuffer, Rgb, RgbImage};
+use image::{GenericImage, ImageBuffer, Rgb, RgbImage, SubImage};
 use ndarray::{s, Array2};
 use rand::{thread_rng, Rng};
 use show_image::{
@@ -14,158 +14,156 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
   let mut rng = thread_rng();
+
   let map = Map::gen_map(200, 100);
 
-  let mut people: Vec<_> = (0..50)
+  let people: Vec<_> = (0..50)
     .map(|_| Person {
       brain: Map::gen_brain(200, 100),
       x: rng.gen_range(0..map.width()),
       y: rng.gen_range(0..map.height()),
     })
     .collect();
-  let mut selected_person = 0;
 
-  let map_window = create_window(
-    "Map",
+  let mut state = State {
+    map,
+    people,
+    selected_person: 0,
+  };
+
+  let window = create_window(
+    "Info Distribution",
     WindowOptions {
-      size: Some([1000, 1000]),
+      size: Some([1920, 1080]),
       ..Default::default()
     },
   )?;
-  map_window.set_image("map", map.as_image())?;
+  window.set_image("image", state.draw())?;
 
-  let people_map_window = create_window(
-    "People Map",
-    WindowOptions {
-      size: Some([1000, 1000]),
-      ..Default::default()
-    },
-  )?;
-  people_map_window
-    .set_image("people", map_of_people(&map, &people, selected_person))?;
-
-  let brain_window = create_window(
-    "Brain",
-    WindowOptions {
-      size: Some([1000, 1000]),
-      ..Default::default()
-    },
-  )?;
-  brain_window.set_image("brain", people[selected_person].brain.as_image())?;
-
-  let mse_window = create_window(
-    "MSE",
-    WindowOptions {
-      size: Some([1000, 1000]),
-      ..Default::default()
-    },
-  )?;
-  mse_window.set_image("mse", mse_of_brains(&map, &people))?;
-
-  for event in brain_window.event_channel()? {
+  for event in window.event_channel()? {
     if let WindowEvent::KeyboardInput(event) = event {
       if !event.input.state.is_pressed() {
         continue;
       }
       match event.input.key_code {
-        event @ Some(VirtualKeyCode::Left | VirtualKeyCode::Right) => {
-          match event {
-            Some(VirtualKeyCode::Left) => {
-              selected_person =
-                selected_person.checked_sub(1).unwrap_or(people.len() - 1);
-            }
-            Some(VirtualKeyCode::Right) => {
-              selected_person = (selected_person + 1) % people.len();
-            }
-            _ => {
-              unreachable!()
-            }
-          };
-
-          brain_window
-            .set_image("brain", people[selected_person].brain.as_image())?;
-          people_map_window.set_image(
-            "people",
-            map_of_people(&map, &people, selected_person),
-          )?;
-        }
-        Some(VirtualKeyCode::Space) => {
-          for person in people.iter_mut() {
-            let sense_range = 5;
-            let min_x = person.x.saturating_sub(sense_range);
-            let max_x = (person.x + sense_range).min(map.width());
-
-            let min_y = person.y.saturating_sub(sense_range);
-            let max_y = (person.y + sense_range).min(map.height());
-
-            person
-              .brain
-              .map
-              .slice_mut(s![min_x..max_x, min_y..max_y])
-              .zip_mut_with(
-                &map.map.slice(s![min_x..max_x, min_y..max_y]),
-                |b, m| {
-                  *b = (*b + *m) / 2.0;
-                },
-              );
-          }
-          brain_window
-            .set_image("brain", people[selected_person].brain.as_image())?;
-          mse_window.set_image("mse", mse_of_brains(&map, &people))?;
-        }
-        _ => {}
+        Some(VirtualKeyCode::Escape) => return Ok(()),
+        Some(VirtualKeyCode::Left) => state.select_previous_person(),
+        Some(VirtualKeyCode::Right) => state.select_next_person(),
+        Some(VirtualKeyCode::Space) => state.update(),
+        _ => continue,
       }
+      window.set_image("image", state.draw())?;
     }
   }
 
   Ok(())
 }
 
-fn map_of_people(
-  map: &Map,
-  people: &[Person],
+struct State {
+  map: Map,
+  people: Vec<Person>,
   selected_person: usize,
-) -> RgbImage {
-  let mut img: RgbImage =
-    ImageBuffer::new(map.width() as u32, map.height() as u32);
-
-  for (i, person) in people.iter().enumerate() {
-    *img.get_pixel_mut(person.x as u32, person.y as u32) =
-      if selected_person == i {
-        Rgb([255, 255, 255])
-      } else {
-        Rgb([255, 0, 0])
-      };
-  }
-
-  img
 }
 
-fn mse_of_brains(map: &Map, people: &[Person]) -> RgbImage {
-  let mut mse = people
-    .iter()
-    .map(|p| {
-      let mut e = &p.brain.map - &map.map;
-      e.map_inplace(|v| *v = *v * *v);
-      e
-    })
-    .fold(Array2::<f64>::zeros(map.map.raw_dim()), |mut acc, cur| {
-      acc += &cur;
-      acc
-    });
-  mse /= people.len() as f64;
-
-  let mut img: RgbImage =
-    ImageBuffer::new(mse.shape()[0] as u32, mse.shape()[1] as u32);
-
-  for (x, y, pixel) in img.enumerate_pixels_mut() {
-    let v = mse[(x as usize, y as usize)];
-    let as_u8 = (v * 255.0) as u8;
-
-    *pixel = Rgb([as_u8, as_u8, as_u8]);
+impl State {
+  fn select_previous_person(&mut self) {
+    self.selected_person = self
+      .selected_person
+      .checked_sub(1)
+      .unwrap_or(self.people.len() - 1);
   }
 
-  img
+  fn select_next_person(&mut self) {
+    self.selected_person = (self.selected_person + 1) % self.people.len();
+  }
+
+  fn update(&mut self) {
+    for person in self.people.iter_mut() {
+      let sense_range = 5;
+      let min_x = person.x.saturating_sub(sense_range);
+      let max_x = (person.x + sense_range).min(self.map.width());
+
+      let min_y = person.y.saturating_sub(sense_range);
+      let max_y = (person.y + sense_range).min(self.map.height());
+
+      person
+        .brain
+        .map
+        .slice_mut(s![min_x..max_x, min_y..max_y])
+        .zip_mut_with(
+          &self.map.map.slice(s![min_x..max_x, min_y..max_y]),
+          |b, m| {
+            *b = (*b + *m) / 2.0;
+          },
+        );
+    }
+  }
+
+  fn draw(&self) -> RgbImage {
+    let mut img = ImageGrid::new(self.map.width(), self.map.height(), 2, 2);
+
+    {
+      let mut buffer = img.grid_mut(0, 0);
+
+      self.map.draw(&mut buffer);
+
+      for (i, person) in self.people.iter().enumerate() {
+        *buffer.get_pixel_mut(person.x as u32, person.y as u32) =
+          if self.selected_person == i {
+            Rgb([0, 255, 0])
+          } else {
+            Rgb([255, 0, 0])
+          };
+      }
+    }
+
+    {
+      let mut buffer = img.grid_mut(0, 1);
+
+      let mut mse = self
+        .people
+        .iter()
+        .map(|p| {
+          let mut e = &p.brain.map - &self.map.map;
+          e.map_inplace(|v| *v = *v * *v);
+          e
+        })
+        .fold(
+          Array2::<f64>::zeros(self.map.map.raw_dim()),
+          |mut acc, cur| {
+            acc += &cur;
+            acc
+          },
+        );
+      mse /= self.people.len() as f64;
+
+      for ((x, y), v) in mse.indexed_iter() {
+        let as_u8 = (v * 255.0) as u8;
+        *buffer.get_pixel_mut(x as u32, y as u32) = Rgb([as_u8, as_u8, as_u8]);
+      }
+    }
+
+    let selected_person = &self.people[self.selected_person];
+
+    {
+      let mut buffer = img.grid_mut(1, 0);
+
+      selected_person.brain.draw(&mut buffer);
+    }
+
+    {
+      let mut buffer = img.grid_mut(1, 1);
+
+      let error = (&selected_person.brain.map - &self.map.map).map(|v| v.abs());
+
+      for ((x, y), v) in error.indexed_iter() {
+        let as_u8 = (v * 255.0) as u8;
+        *buffer.get_pixel_mut(x as u32, y as u32) = Rgb([as_u8, as_u8, as_u8]);
+      }
+    }
+
+    img.into_inner()
+  }
 }
 
 struct Map {
@@ -201,18 +199,11 @@ impl Map {
     self.map.shape()[1]
   }
 
-  fn as_image(&self) -> RgbImage {
-    let mut img: RgbImage =
-      ImageBuffer::new(self.width() as u32, self.height() as u32);
-
-    for (x, y, pixel) in img.enumerate_pixels_mut() {
-      let v = self.map[(x as usize, y as usize)];
+  fn draw(&self, img: &mut SubImage<&mut RgbImage>) {
+    for ((x, y), v) in self.map.indexed_iter() {
       let as_u8 = (v * 255.0) as u8;
-
-      *pixel = Rgb([as_u8, as_u8, as_u8]);
+      *img.get_pixel_mut(x as u32, y as u32) = Rgb([as_u8, as_u8, as_u8]);
     }
-
-    img
   }
 }
 
@@ -223,3 +214,32 @@ struct Person {
 }
 
 impl Person {}
+
+struct ImageGrid {
+  img: RgbImage,
+  width: usize,
+  height: usize,
+}
+
+impl ImageGrid {
+  fn new(width: usize, height: usize, c: usize, r: usize) -> Self {
+    let img: RgbImage = ImageBuffer::new(
+      (c * width + (c - 1)) as u32,
+      (r * height + (r - 1)) as u32,
+    );
+    Self { img, width, height }
+  }
+
+  fn into_inner(self) -> RgbImage {
+    self.img
+  }
+
+  fn grid_mut(&mut self, c: usize, r: usize) -> SubImage<&mut RgbImage> {
+    self.img.sub_image(
+      (c * (self.width + 1)) as u32,
+      (r * (self.height + 1)) as u32,
+      self.width as u32,
+      self.height as u32,
+    )
+  }
+}
